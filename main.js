479167043c3d54d18457be8d0e12a5305f27cc53
MCluck <mcluck90@gmail.com>
2013-10-17 19:16:57 -0600

    Changed main to take command-line arguments for the source file

--- main.js
+++ main.js
@@ -1,27 +1,50 @@
+/**
+ * Generate a Psykick Component from a simplified syntax
+ * Usage: node main.js inputfile
+ */
+
 'use strict';
 
+if (process.argv.length !== 3) {
+    console.log('Usage: node main.js inputfile');
+    process.exit(1);
+}
+
 var PEG = require('pegjs'),
+    Generator = require('./generator.js'),
     fs = require('fs'),
     loadFile = function(path) {
         return fs.readFileSync(path).toString();
-    },
-    source = loadFile('./examples/basic.component'),
-    parser = PEG.buildParser(loadFile('./grammar.pegjs'));
+    };
 
-var Generator = require('./generator.js'),
-    generator = new Generator(parser.parse(source)),
-    components = generator.generateCode();
+
+// Generate a parser
+var parser = PEG.buildParser(loadFile('./grammar.pegjs'));
+
+// Load the source file
+var source = loadFile(process.argv[2]);
+
+// Create a code generator
+var generator = new Generator(parser.parse(source));
+
+// Generate code for each of the components
+var components = generator.generateCode();
 
 for (var i = 0, len = components.length; i < len; i++) {
-    var defaultFilename = components[i].name
-        .replace(/component/gi, '')
-        .replace(/^[A-Z]/, function(c) {
-            return c.toLowerCase();
-        })
-        .replace(/[A-Z]/g, function(c) {
-            return '-' + c.toLowerCase();
-        }) + '.js';
-    console.log(defaultFilename);
-    console.log(components[i].code);
-    console.log('');
+    // Create a filename by converting the component name from
+    // PascalCase into a dash-separated-style and removing 'Component' from the name
+    var component = components[i],
+        filename = component.name
+            .replace(/component/gi, '')
+            .replace(/^[A-Z]/, function(c) {
+                return c.toLowerCase();
+            })
+            .replace(/[A-Z]/g, function(c) {
+                return '-' + c.toLowerCase();
+            }) + '.js';
+
+    fs.writeFileSync(filename, component.code);
+    console.log('Compiled ' + filename);
 }
+
+console.log('Done!');
